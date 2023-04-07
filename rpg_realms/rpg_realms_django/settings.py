36755b8cfb21f41c09ffdde07c9b4e1dc2acf244
Austin <austinih46@gmail.com>
2023-04-07 16:42:23 -0500

    updated Database

--- rpg_realms/rpg_realms_django/settings.py
+++ rpg_realms/rpg_realms_django/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import dj_database_url
 
+
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
