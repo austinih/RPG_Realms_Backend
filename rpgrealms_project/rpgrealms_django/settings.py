b4d5819b2c772456ee5b73f3414a2a9c43059fa2
Austin <austinih46@gmail.com>
2023-04-08 15:33:20 -0500

    deploy

--- rpgrealms_project/rpgrealms_django/settings.py
+++ rpgrealms_project/rpgrealms_django/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure-9^da4^t%ktyio%8*px&iz^4k+3=&min&a*uka3dwj7p$ba#+%g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
